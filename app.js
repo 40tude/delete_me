f824bc291a62da61afa4b7925966188fb4446fc3
Philippe BAUCOUR <philippe.baucour@gmail.com>
2023-11-06 08:44:42 +0100

    Edit

---
+++ app.js
@@ -0,0 +1,32 @@
+const variable = "Test"
+
+SlowFunction()
+
+function SlowFunction(){
+
+    // sdfsfd
+    console.log("Hello")
+    
+}
+
+
+function MediumFunction(){
+
+    // sdfsfd
+    console.log("Hello")
+
+}
+
+function FastFunction(){
+
+    // sdfsfd
+    console.log("Hello")
+
+}
+
+function VeryFastFunction(){
+
+    // sdfsfd
+    console.log("Hello")
+
+}
